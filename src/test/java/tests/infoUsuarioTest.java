47d83eaa0825a3b09b8a9a7dc7fff3e6e1e153f8
Mesch <mesch4@gmail.com>
2020-04-27 20:52:52 -0300

    version 1.0

---
+++ src/test/java/tests/infoUsuarioTest.java
@@ -0,0 +1,48 @@
+package tests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class infoUsuarioTest {
+    @Test
+    public void testAdicionarUmaInformacaoAdicionalDoUsuario() {
+
+        // Abrindo o Navegador
+        System.setProperty("webdriver.chrome.driver","C:\\dev\\drivers\\chromedriver.exe");
+        WebDriver navegador = new ChromeDriver();
+        navegador.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // Navegando
+        navegador.get("http://www.juliodelima.com.br/taskit");
+
+        // Clicar no link que possui o texto "Sign in"
+        navegador.findElement(By.linkText("Sign in")).click();
+
+        // Identificando o formulário de Login
+        WebElement formularioSignInBox = navegador.findElement(By.id("signinbox"));
+
+        // Digitar no campo com name "login" que está dentro do formulário de id "signinbox" o texto "julio0001"
+        formularioSignInBox.findElement(By.name("login")).sendKeys("julio0001");
+
+        // Digitar no campo com name "password" que está dentro do formulário de id "signinbox" o texto "123456"
+        formularioSignInBox.findElement(By.name("password")).sendKeys("123456");
+
+        // Clicar no link com o texto "SIGN IN"
+        navegador.findElement(By.linkText("SIGN IN")).click();
+
+        // Validar que dentro do elemento com class "me" está o texto "Hi, Julio"
+        WebElement me = navegador.findElement(By.className("me"));
+        String textoNoElementoMe = me.getText();
+        assertEquals("Hi, Julio",textoNoElementoMe);
+
+        // Fechar o navegador
+        navegador.quit();
+
+    }
+}
