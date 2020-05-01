05d3f1be188ec0cab0bc0ab873bb50c20c9009d3
Mesch <mesch4@gmail.com>
2020-05-01 18:44:03 -0300

    version 1.0

--- src/test/java/tests/addInfoUsuarioTest.java
+++ src/test/java/tests/addInfoUsuarioTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.concurrent.TimeUnit;
 
@@ -44,12 +45,37 @@ public class addInfoUsuarioTest {
         // Clicar no link com o texto "SIGN IN"
         navegador.findElement(By.linkText("SIGN IN")).click();
 
-        
+        // Clicar em um link que possui a class "me"
+        navegador.findElement(By.className("me")).click();
+
+        // Clicar em um link que possui o texto "MORE DATA ABOUT YOU"
+        navegador.findElement(By.linkText("MORE DATA ABOUT YOU")).click();
+
+        // Clicar no botao atraves do seu xpath (//button[@data-target="addmoredata"])
+        navegador.findElement(By.xpath("//button[@data-target=\"addmoredata\"]")).click();
+
+        // Identificar a popup onde esta o formulario de id "addmoredata"
+        WebElement popupAddMoreData = navegador.findElement(By.id("addmoredata"));
+
+        // Na combo de name "type" escolher a opcao "Phone"
+        WebElement campoType = popupAddMoreData.findElement(By.name("type"));
+        new Select(campoType).selectByVisibleText("Phone");
+
+        // No campo de name "contact" digitar "+5511999999999"
+        popupAddMoreData.findElement(By.name("contact")).sendKeys("+5511999999999");
+
+        // Clicar no link de text "SAVE" que esta na popup
+        popupAddMoreData.findElement(By.linkText("SAVE")).click();
+
+        // Na mensagem de id "toast-container" validar que o texto é "Your contact has been added!"
+        WebElement mensagemPop = navegador.findElement(By.id("toast-container"));
+        String mensagem = mensagemPop.getText();
+        assertEquals("Your contact has been added!",mensagem);
 
     }
     @After
     public void tearDown() {
         // Fechar o navegador
-        navegador.quit();
+        // navegador.quit();
     }
 }
